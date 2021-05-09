57152b058f54f0506a5e081994c8b5e16eeb1ae2
Novaldi Sandi Ago <75278889+DeadpoolSteinS@users.noreply.github.com>
2021-05-09 12:13:06 +0700

    Add files via upload

---
+++ Problems/PSLTOKI1/rubar.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mp make_pair
+#define pb push_back
+
+int main(){
+	ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int N, baru = 0;
+	cin >> N;
+	int X[N], Y[N];
+	vector <int> rumahx, rumahy;
+	vector <pair <int, int>> ex, ye;
+	for(int i = 0; i < N; i++)
+		cin >> X[i];
+	for(int i = 0; i < N; i++)
+		cin >> Y[i];
+	for(int i = 0; i < N; i++)
+		for(int j = i+1; j < N; j++){
+			if(X[i] == X[j]){
+				ex.pb(mp(min(Y[i], Y[j]), max(Y[i], Y[j])));
+				rumahx.pb(X[i]);
+			}
+			if(Y[i] == Y[j]){
+				ye.pb(mp(min(X[i], X[j]), max(X[i], X[j])));
+				rumahy.pb(Y[i]);
+			}
+		}
+	for(int i = 0; i < rumahx.size(); i++)
+		for(int j = i+1; j < rumahx.size(); j++)
+			if(rumahx[i] == rumahx[j]){
+				ex[i] = (mp(min(ex[i].first, ex[j].first), max(ex[i].second, ex[j].second)));
+				ex.erase(ex.begin() + j);
+				rumahx.erase(rumahx.begin() + j);
+				j--;
+			}
+	for(int i = 0; i < rumahy.size(); i++)
+		for(int j = i+1; j < rumahy.size(); j++)
+			if(rumahy[i] == rumahy[j]){
+				ye[i] = (mp(min(ye[i].first, ye[j].first), max(ye[i].second, ye[j].second)));
+				ye.erase(ye.begin() + j);
+				rumahy.erase(rumahy.begin() + j);
+				j--;
+			}
+	for(int i = 0; i < rumahx.size(); i++)
+		for(int j = 0; j < rumahy.size(); j++)
+			if(rumahy[j] > ex[i].first && rumahy[j] < ex[i].second && rumahx[i] > ye[j].first && rumahx[i] < ye[j].second)
+				baru++;
+	cout << baru << endl;
+	return 0;
+}
