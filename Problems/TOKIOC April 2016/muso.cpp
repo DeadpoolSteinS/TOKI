cd1b5b532c6e26bf4ab3304b02000a9f13712a9c
Novaldi Sandi Ago <75278889+DeadpoolSteinS@users.noreply.github.com>
2021-08-04 15:02:52 +0700

    Add files via upload

---
+++ Problems/TOKIOC April 2016/muso.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll unsigned long long
+
+int main(){
+	ll X, Y;
+	cin >> X >> Y;
+	cout << (ll)((ll)(X+Y)%100044203905979) << endl;
+	return 0;
+}
